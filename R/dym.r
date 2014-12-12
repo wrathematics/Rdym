5458d3b6068f8fe9446c17a59f2b41c332d92fd3
Homer White <hwhite0@georgetowncollege.edu>
2014-12-11 23:06:34 -0500

    dealt with mis-named packages

--- R/dym.r
+++ R/dym.r
@@ -1,8 +1,20 @@
-did_you_mean <- function(name, lastcall)
+did_you_mean <- function(name, lastcall,problem)
 {
-  
   name <- sub(x=name, pattern="\\n", replacement="")
   with.namespace <- length(grep(x=name, pattern="::")) > 0
+  with.data <- length(grep(x=lastcall, pattern="data = ")) > 0
+  
+  if (problem=="package") {
+    objs <- installed.packages()[,"Package"]
+    closest <- find_closest_word(name, objs)
+    word <- closest$word
+    cat(paste0("\nDid you mean:  ", word, "  ?\n"))
+    if (!missing(lastcall))
+    {
+      cat(paste0(sub(x=lastcall, pattern=name, replacement=word), "\n"))
+    }
+    return(invisible())
+  }
   
   if (with.namespace)
   {
@@ -14,6 +26,16 @@ did_you_mean <- function(name, lastcall)
   {
     objs <- lapply(search(), objects)
     objs <- unique(c(ls(), do.call(c, objs)))
+  }
+  
+  if (with.data)
+  {
+    dataObj <- sub(x=lastcall, pattern=".*data = ", replacement="")
+    dataObj <- sub(x=dataObj, pattern="[,)].*", replacement="")
+    
+    if (name != dataObj) {
+      objs <- c(names(get(dataObj,inherits=T)),objs)
+    }
   }
   
   closest <- find_closest_word(name, objs)
--- R/error.r
+++ R/error.r
@@ -18,30 +18,68 @@ get_lastcall <- function()
 
 # Let's try using .Traceback, in case user spread call over
 # several lines
-
-  stack <- .Traceback
+  
+  ## capture all the output to a file.
+  dumpFile <- file("all.Rout", open = "wt")
+  sink(dumpFile,type="output")
+  stack <- traceback(1)
+  sink(type="message")
+  sink()
+  close(dumpFile)
+  unlink("all.Rout")
   return(stack[[length(stack)]])
 
 }
 
+get_lastcall_func <- function(name)
+{
+  Rhistfile <- tempfile(".Rhistory")
+  savehistory(Rhistfile)
+  Rhist <- readLines(Rhistfile)
+  unlink(Rhistfile)
+  
+  m <- length(Rhist)
+  current <- Rhist[m]
+  place <- m
+  while (length(grep(x=current,pattern=name))==0 ){
+    place <- place -1 
+    current <- paste0(Rhist[place],current)
+  }
+  command <- gsub(x=current,pattern=paste0(".*",name),replace=name)
+  return(command)
+}
+
+
 stop_dym <- function()
 {
-  lastcall <- get_lastcall()
   
+  lastcall <- get_lastcall()
   ### Handle error message
   msg <- geterrmessage()
+  
+  
   
   if (matcherr(msg=msg, pattern="could not find function") || matcherr(msg=msg, pattern="is not an exported object from"))
   {
     fun <- sub(x=msg, pattern=".*could not find function \"", replacement="")
     fun <- sub(x=fun, pattern="\"", replacement="")
-    did_you_mean(fun, lastcall)
+    fun <- sub(x=fun, pattern="\\n", replacement="")
+    
+    lastcall <- get_lastcall_func(fun)
+    did_you_mean(fun, lastcall,problem="function")
   }
   else if (matcherr(msg=msg, pattern="not found"))
   {
     obj <- sub(x=msg, pattern=".*object '", replacement="")
     obj <- sub(x=obj, pattern="' not found\\n", replacement="")
-    did_you_mean(obj, lastcall)
+    did_you_mean(obj, lastcall,problem="object")
+  }
+  else if (matcherr(msg=msg, pattern="there is no package called"))
+  {
+    pack <- sub(x=msg, pattern=".*there is no package called ", replacement="")
+    pack <- sub(x=pack, pattern="\\n", replacement="")
+    pack <- gsub(pack,pattern="[[:punct:]]",replace="")
+    did_you_mean(pack, lastcall,problem="package")
   }
   else if (matcherr(msg=msg, pattern="unused argument(?s)"))
   {
